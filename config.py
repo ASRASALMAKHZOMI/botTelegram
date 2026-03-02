5cf524995a1ea40aff854751388b4b29a759acf2
botAlmakhzomi <abdullahalmakhzomi2018@gmail.com>
2026-03-02 17:11:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ if not DATABASE_URL:
 # System Settings
 # =========================
 
-MAINTENANCE_MODE = True
+MAINTENANCE_MODE = False
 ADMIN_ID = "6829734732"
 
 # =========================
@@ -31,4 +31,5 @@ LEVEL2_FOLDER = "Files"
 LEVEL3_FOLDER = "Level 3"
 
 LEVEL4_FOLDER = "Level 4"
+
 
